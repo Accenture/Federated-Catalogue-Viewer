4f89a40a424c40004afc8307e00d73c78d7f7e2d
Christoph Becker <christoph.a.becker@accenture.com>
2023-09-04 15:12:16 +0200

    feat: rebrand to accenture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+    protected readonly toolbar = toolbar;
+
+    constructor(private router: Router) {}
+    getActiveClass(urlPrefix: string): Array<string> {
+        console.log(this.router.url);
+        if (this.router.url.startsWith(urlPrefix)) {
+            return ['active'];
+        }
+        return [];
+    }
+}
