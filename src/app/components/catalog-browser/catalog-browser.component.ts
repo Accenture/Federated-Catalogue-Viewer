43f58bd658710f5cfe5e53a544955a78038ecfd2
John Hoffmann <john.hoffmann@accenture.com>
2024-08-21 13:56:41 +0200

    feat: add tabs for LegalParticipants, ServiceOfferings, Resources

--- src/app/components/catalog-browser/catalog-browser.component.ts
+++ src/app/components/catalog-browser/catalog-browser.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NodeQueryResult, QueryResponse } from '../../types/dtos';
 import { QueryService } from '../../services/query.service';
 import { ActivatedRoute } from '@angular/router';
-import { map, Observable, switchMap } from 'rxjs';
+import { map, Observable, forkJoin } from 'rxjs';
 
 @Component({
     selector: 'app-catalog-browser',
@@ -10,13 +10,31 @@ import { map, Observable, switchMap } from 'rxjs';
     styleUrls: ['./catalog-browser.component.scss'],
 })
 export class CatalogBrowserComponent implements OnInit {
-    data?: QueryResponse<NodeQueryResult>;
+    data: Record<string, QueryResponse<NodeQueryResult> | undefined> = {};
     label?: Observable<string>;
+    selectedTab = 0;
 
     constructor(private _queryService: QueryService, private _activatedRoute: ActivatedRoute) {}
+
     ngOnInit(): void {
         this.label = this._activatedRoute.queryParams.pipe(map((params) => params['label']));
 
-        this.label.pipe(switchMap(this._queryService.allNodes)).subscribe((values) => (this.data = values));
+        forkJoin({
+            LegalParticipant: this._queryService.allNodes('LegalParticipant'),
+            ServiceOffering: this._queryService.allNodes('ServiceOffering'),
+            Resource: this._queryService.allNodes('Resource'),
+        }).subscribe((results) => {
+            this.data['LegalParticipant'] = results.LegalParticipant;
+            this.data['ServiceOffering'] = results.ServiceOffering;
+            this.data['Resource'] = results.Resource;
+        });
+    }
+
+    onTabChange(index: number): void {
+        this.selectedTab = index;
+    }
+
+    getTabKey(): string {
+        return ['LegalParticipant', 'ServiceOffering', 'Resource'][this.selectedTab];
     }
 }
