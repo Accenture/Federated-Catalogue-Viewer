aa2f44ad6b05b6b4bc0300928ce09dd2aa9de994
John Hoffmann <john.hoffmann@accenture.com>
2024-10-30 12:26:05 +0100

    feat: add marketplace boilerplate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { QueryComponent } from './components/query/query.component';
 import { CatalogBrowserComponent } from './components/catalog-browser/catalog-browser.component';
 import { NodeDetailsComponent } from './components/node-details/node-details.component';
+import { MarketplaceComponent } from './components/marketplace/marketplace.component';
 
 export const routes: Routes = [
     { path: 'nodes', component: CatalogBrowserComponent },
     { path: 'query', component: QueryComponent },
+    { path: 'marketplace', component: MarketplaceComponent },
     { path: 'nodes/:id', component: NodeDetailsComponent },
     { path: '', redirectTo: 'nodes', pathMatch: 'full' },
 ];
