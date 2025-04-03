6be0d7abc0f5726392eb2ab293d4c63ac1cd8598
John Hoffmann <john.hoffmann@accenture.com>
2025-04-03 17:06:40 +0200

    feat: retain asset relations & group resources

--- src/app/components/marketplace/marketplace.component.ts
+++ src/app/components/marketplace/marketplace.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Observable, EMPTY, catchError } from 'rxjs';
-import { ServiceCard } from '../../types/dtos';
+import { Observable, EMPTY, catchError, tap } from 'rxjs';
+import { Resource, ServiceCard } from '../../types/dtos';
 import { MarketplaceService } from '../../services/marketplace.service';
 import { DataFormattingService } from '../../services/data-formatting.service';
 import { QueryService } from 'src/app/services/query.service';
@@ -13,10 +13,11 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { HttpErrorResponse } from '@angular/common/http';
 
-interface CarouselIndexes {
+export interface CarouselIndexes {
     dataResourceIndex: number;
     serviceAccessPointIndex: number;
     physicalResourceIndex: number;
+    groupId?: number;
 }
 
 enum IndexType {
@@ -65,6 +66,9 @@ export class MarketplaceComponent implements OnInit {
 
     fetchData(): void {
         this.services$ = this.marketplaceService.fetchServiceData(this.legalName).pipe(
+            tap((services: ServiceCard[]) => {
+                services.forEach((service) => this.getCarouselIndexes(service.serviceOffering.id));
+            }),
             catchError((error: HttpErrorResponse) => {
                 this.error = error;
                 return EMPTY;
@@ -102,15 +106,63 @@ export class MarketplaceComponent implements OnInit {
         return this.serviceIndexes.get(serviceId) as CarouselIndexes;
     }
 
-    updateIndex(serviceId: number, indexType: IndexType, resources: unknown[], direction: 'next' | 'previous'): void {
+    updateIndex(
+        serviceId: number,
+        indexType: IndexType,
+        resources: (Resource & { groupIds?: number[] })[],
+        direction: 'next' | 'previous',
+    ): void {
         const indexes = this.getCarouselIndexes(serviceId);
-        const currentIndex = indexes[indexType];
-        const maxIndex = resources.length - 1;
 
-        if (direction === 'next') {
-            indexes[indexType] = currentIndex < maxIndex ? currentIndex + 1 : 0;
+        const groupedResources = resources.filter((resource) => resource.groupIds && resource.groupIds.length > 0);
+        const groupIds = [...new Set(groupedResources.flatMap((resource) => resource.groupIds || []))]
+            .filter((id): id is number => id !== undefined)
+            .sort((a, b) => a - b);
+
+        if (groupIds.length > 0) {
+            const currentGroupId = groupIds[indexes[indexType]] || groupIds[0];
+            const currentIndex = groupIds.indexOf(currentGroupId);
+
+            let newIndex;
+            if (direction === 'next') {
+                newIndex = currentIndex < groupIds.length - 1 ? currentIndex + 1 : 0;
+            } else {
+                newIndex = currentIndex > 0 ? currentIndex - 1 : groupIds.length - 1;
+            }
+
+            const newGroupId = groupIds[newIndex];
+
+            Object.values(IndexType).forEach((type) => {
+                indexes[type] = groupIds.indexOf(newGroupId);
+            });
         } else {
-            indexes[indexType] = currentIndex > 0 ? currentIndex - 1 : maxIndex;
+            const currentIndex = indexes[indexType];
+            const maxIndex = resources.length - 1;
+
+            if (direction === 'next') {
+                indexes[indexType] = currentIndex < maxIndex ? currentIndex + 1 : 0;
+            } else {
+                indexes[indexType] = currentIndex > 0 ? currentIndex - 1 : maxIndex;
+            }
         }
+    }
+
+    getMaxGroupCount(service: ServiceCard): number {
+        const allResources = [
+            ...(service.dataResources || []),
+            ...(service.serviceAccessPoints || []),
+            ...(service.physicalResources || []),
+        ];
+
+        const groupIds = new Set(
+            allResources.filter((r) => r.groupIds && r.groupIds.length > 0).flatMap((r) => r.groupIds as number[]),
+        );
+
+        return groupIds.size || 1;
+    }
+
+    getCurrentResource<T extends Resource>(resources: T[] | undefined, index: number | undefined): T | null {
+        if (!resources || index === undefined || index < 0 || index >= resources.length) return null;
+        return resources[index];
     }
 }
--- src/app/services/service-data.service.ts
+++ src/app/services/service-data.service.ts
@@ -1,5 +1,18 @@
 import { Injectable } from '@angular/core';
-import { NodeQueryResult, ServiceCard, Resource, DataResource } from '../types/dtos';
+import {
+    NodeQueryResult,
+    ServiceCard,
+    Resource,
+    DataResource,
+    PhysicalResource,
+    ServiceAccessPoint,
+    AssetNode,
+    LegalParticipant,
+    LegalRegistrationNumber,
+    ServiceOffering,
+    Asset,
+} from '../types/dtos';
+import { isEqual } from 'lodash';
 
 @Injectable({
     providedIn: 'root',
@@ -15,83 +28,227 @@ export class CardDataService {
             dataResources: [],
         };
 
+        const nodeClassesToGroup = [
+            'DataResource',
+            'InstantiatedVirtualResource',
+            'ServiceAccessPoint',
+            'PhysicalResource',
+        ];
+        const nodesToGroup: AssetNode[] = [];
+
+        const assetsMap = new Map<number, { id: number; type: string; asset: Asset }>();
+
         items.forEach((item) => {
+            if (item.labels) {
+                const matchedLabel = item.labels.find((label: string) => nodeClassesToGroup.includes(label));
+
+                if (matchedLabel && item.relation) {
+                    nodesToGroup.push({
+                        assetId: item.id,
+                        linkedAssetId: item.relation.id,
+                        assetLabel: matchedLabel,
+                    });
+                }
+            }
+
             if (item.labels.includes('LegalParticipant') && item.value['legalName']) {
-                serviceCard.legalParticipant = {
-                    id: item.id,
-                    name: item.value['legalName'] as string,
-                    description: (item.value['description'] as string[]) || [],
-                };
+                if (!assetsMap.has(item.id)) {
+                    const legalParticipant: LegalParticipant = {
+                        id: item.id,
+                        name: item.value['legalName'] as string,
+                        description: (item.value['description'] as string[]) || [],
+                    };
+                    serviceCard.legalParticipant = legalParticipant;
+                    assetsMap.set(item.id, { id: item.id, type: 'LegalParticipant', asset: legalParticipant });
+                }
             }
 
             if (item.labels.includes('legalRegistrationNumber')) {
-                serviceCard.legalRegistrationNumber = {
-                    id: item.id,
-                    description: (item.value['description'] as string[]) || [],
-                    vatID: (item.value['vatID'] as string) || '',
-                    leiCode: (item.value['leiCode'] as string) || '',
-                    countryCode:
-                        (item.value['vatID-countryCode'] as string) ||
-                        (item.value['leiCode-countryCode'] as string) ||
-                        '',
-                };
-                serviceCard.legalRegistrationNumber['subdivisionCountryCode'] =
-                    (item.value['vatID-subdivisionCountryCode'] as string) ||
-                    (item.value['leiCode-subdivisionCountryCode'] as string) ||
-                    '';
+                if (!assetsMap.has(item.id)) {
+                    const legalRegistrationNumber: LegalRegistrationNumber = {
+                        id: item.id,
+                        description: (item.value['description'] as string[]) || [],
+                        vatID: (item.value['vatID'] as string) || '',
+                        leiCode: (item.value['leiCode'] as string) || '',
+                        countryCode:
+                            (item.value['vatID-countryCode'] as string) ||
+                            (item.value['leiCode-countryCode'] as string) ||
+                            '',
+                        subdivisionCountryCode:
+                            (item.value['vatID-subdivisionCountryCode'] as string) ||
+                            (item.value['leiCode-subdivisionCountryCode'] as string) ||
+                            '',
+                    };
+                    serviceCard.legalRegistrationNumber = legalRegistrationNumber;
+                    assetsMap.set(item.id, {
+                        id: item.id,
+                        type: 'legalRegistrationNumber',
+                        asset: legalRegistrationNumber,
+                    });
+                }
             }
 
             if (item.labels.includes('ServiceOffering') && item.value['name']) {
-                serviceCard.serviceOffering = {
-                    id: item.id,
-                    name: item.value['name'] as string,
-                    description: (item.value['description'] as string[]) || [],
-                    policy: (item.value['policy'] as string[]) || [],
-                };
+                if (!assetsMap.has(item.id)) {
+                    const serviceOffering: ServiceOffering = {
+                        id: item.id,
+                        name: item.value['name'] as string,
+                        description: (item.value['description'] as string[]) || [],
+                        policy: (item.value['policy'] as string[]) || [],
+                    };
+                    serviceCard.serviceOffering = serviceOffering;
+                    assetsMap.set(item.id, { id: item.id, type: 'ServiceOffering', asset: serviceOffering });
+                }
             }
 
             if (item.labels.includes('ServiceAccessPoint') && item.value['host'] && item.value['port']) {
-                const serviceAccessPoint = {
-                    id: item.id,
-                    host: item.value['host'] as string,
-                    openAPI: (item.value['openAPI'] as string) || '',
-                    port:
-                        typeof item.value['port'] === 'string'
-                            ? parseInt(item.value['port'] as string, 10)
-                            : (item.value['port'] as number),
-                    protocol: (item.value['protocol'] as string) || '',
-                    version: (item.value['version'] as string) || '',
-                    description: (item.value['description'] as string[]) || [],
-                };
-                serviceCard.serviceAccessPoints?.push(serviceAccessPoint);
+                if (!assetsMap.has(item.id)) {
+                    const serviceAccessPoint: ServiceAccessPoint = {
+                        id: item.id,
+                        host: item.value['host'] as string,
+                        openAPI: (item.value['openAPI'] as string) || '',
+                        port:
+                            typeof item.value['port'] === 'string'
+                                ? parseInt(item.value['port'] as string, 10)
+                                : (item.value['port'] as number),
+                        protocol: (item.value['protocol'] as string) || '',
+                        version: (item.value['version'] as string) || '',
+                        description: (item.value['description'] as string[]) || [],
+                    };
+                    serviceCard.serviceAccessPoints?.push(serviceAccessPoint);
+                    assetsMap.set(item.id, { id: item.id, type: 'ServiceAccessPoint', asset: serviceAccessPoint });
+                }
             }
 
             if (item.labels.includes('PhysicalResource') && item.value['license'] && item.value['location']) {
-                const physicalResource = {
-                    id: item.id,
-                    name: (item.value['name'] as string) || '',
-                    description: (item.value['description'] as string[]) || [],
-                    license: (item.value['license'] as string) || '',
-                    location: (item.value['location'] as string) || '',
-                    policy: (item.value['policy'] as string[]) || [],
-                };
-                serviceCard.physicalResources?.push(physicalResource);
+                if (!assetsMap.has(item.id)) {
+                    const physicalResource: PhysicalResource = {
+                        id: item.id,
+                        name: (item.value['name'] as string) || '',
+                        description: (item.value['description'] as string[]) || [],
+                        license: (item.value['license'] as string) || '',
+                        location: (item.value['location'] as string) || '',
+                        policy: (item.value['policy'] as string[]) || [],
+                    };
+                    serviceCard.physicalResources?.push(physicalResource);
+                    assetsMap.set(item.id, { id: item.id, type: 'PhysicalResource', asset: physicalResource });
+                }
             }
 
             if (item.labels.includes('DataResource')) {
-                const dataResource: DataResource = {
-                    id: item.id,
-                    name: (item.value['name'] as string) || '',
-                    description: (item.value['description'] as string[]) || [],
-                    containsPII: (item.value['containsPII'] as string) || '',
-                    copyrightOwnedBy: (item.value['copyrightOwnedBy'] as string) || '',
-                    license: (item.value['license'] as string) || '',
-                    policy: (item.value['policy'] as string) || '',
-                };
-                serviceCard.dataResources?.push(dataResource);
+                if (!assetsMap.has(item.id)) {
+                    const dataResource: DataResource = {
+                        id: item.id,
+                        name: (item.value['name'] as string) || '',
+                        description: (item.value['description'] as string[]) || [],
+                        containsPII: (item.value['containsPII'] as string) || '',
+                        copyrightOwnedBy: (item.value['copyrightOwnedBy'] as string) || '',
+                        license: (item.value['license'] as string) || '',
+                        policy: (item.value['policy'] as string) || '',
+                    };
+                    serviceCard.dataResources?.push(dataResource);
+                    assetsMap.set(item.id, { id: item.id, type: 'DataResource', asset: dataResource });
+                }
             }
         });
 
+        const specificGroups2 = this.groupSpecificAssets(nodesToGroup);
+
+        let groupId = 1;
+        specificGroups2.forEach((group) => {
+            Object.values(group).forEach((groupedNode) => {
+                if (groupedNode) {
+                    const asset = assetsMap.get(groupedNode.assetId);
+                    if (asset) {
+                        asset.asset.groupIds = asset.asset.groupIds || [];
+
+                        if (!asset.asset.groupIds.includes(groupId)) {
+                            asset.asset.groupIds.push(groupId);
+                        }
+                    }
+                }
+            });
+            groupId++;
+        });
+
+        serviceCard.dataResources = this.expandResourcesByGroups(serviceCard.dataResources);
+        serviceCard.serviceAccessPoints = this.expandResourcesByGroups(serviceCard.serviceAccessPoints);
+        serviceCard.physicalResources = this.expandResourcesByGroups(serviceCard.physicalResources);
         return serviceCard;
+    }
+
+    private expandResourcesByGroups<T extends Resource>(
+        resources: (T & { groupIds?: number[] })[] = [],
+    ): (T & { groupIds?: number[] })[] {
+        // expands resources with multiple group IDs into separate instances for each group
+        const expanded: (T & { groupIds?: number[] })[] = [];
+        resources.forEach((resource) => {
+            if (resource.groupIds && resource.groupIds.length > 0) {
+                resource.groupIds.forEach((groupId) => {
+                    expanded.push({ ...resource, groupIds: [groupId] });
+                });
+            } else {
+                expanded.push(resource);
+            }
+        });
+        return expanded;
+    }
+
+    private groupSpecificAssets(assetsToGroup: AssetNode[]): Record<string, AssetNode | undefined>[] {
+        // Group related assets
+        const groups: Record<string, AssetNode | undefined>[] = [];
+
+        const findLinkedAssets = (baseAsset: { assetId: number; linkedAssetId: number; assetLabel: string }) =>
+            assetsToGroup.filter(
+                (asset) => asset.assetId === baseAsset.linkedAssetId || asset.linkedAssetId === baseAsset.assetId,
+            );
+
+        assetsToGroup.forEach((baseAsset) => {
+            const visited = new Set<number>();
+            const currentGroup: Record<
+                string,
+                { assetId: number; linkedAssetId: number; assetLabel: string } | undefined
+            > = {};
+
+            const buildGroup = (asset: { assetId: number; linkedAssetId: number; assetLabel: string }) => {
+                if (visited.has(asset.assetId)) return;
+                visited.add(asset.assetId);
+                currentGroup[asset.assetLabel] = asset;
+
+                const linkedAssets = findLinkedAssets(asset);
+                linkedAssets.forEach((linkedAsset) => buildGroup(linkedAsset));
+            };
+
+            buildGroup(baseAsset);
+
+            const sortedGroup = Object.keys(currentGroup)
+                .sort()
+                .reduce((acc, label) => {
+                    acc[label] = currentGroup[label];
+                    return acc;
+                }, {} as typeof currentGroup);
+
+            if (!groups.some((group) => isEqual(group, sortedGroup))) {
+                groups.push(sortedGroup);
+            }
+        });
+
+        const filteredGroups = groups.filter((group, _, allGroups) => {
+            const groupLabels = Object.keys(group).filter((key) => group[key] !== undefined);
+            const groupSet = new Set(groupLabels);
+
+            // Keep only the largest group of linked elements, remove subsets
+            return !allGroups.some((otherGroup) => {
+                const otherLabels = Object.keys(otherGroup).filter((key) => otherGroup[key] !== undefined);
+                const otherSet = new Set(otherLabels);
+
+                if (groupSet.size < otherSet.size && [...groupSet].every((label) => otherSet.has(label))) {
+                    return true;
+                }
+                return false;
+            });
+        });
+
+        return filteredGroups;
     }
 }
