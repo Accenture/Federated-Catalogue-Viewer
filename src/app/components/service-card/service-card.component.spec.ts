d6246164c5ce3df4e826578b09cfb550fadc7069
John Hoffmann <johnvonhoffmann@gmail.com>
2025-05-05 19:33:25 +0200

    fix: init input in test

--- src/app/components/service-card/service-card.component.spec.ts
+++ src/app/components/service-card/service-card.component.spec.ts
@@ -1,10 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ServiceCardComponent } from './service-card.component';
+import { ServiceCard } from '../../types/dtos';
 
 describe('ServiceCardComponent', () => {
     let component: ServiceCardComponent;
     let fixture: ComponentFixture<ServiceCardComponent>;
+
+    const mockService: ServiceCard = {
+        legalParticipant: {
+            id: 1,
+            name: 'Participant',
+        },
+        legalRegistrationNumber: {
+            id: 1,
+            countryCode: 'de',
+        },
+        serviceOffering: {
+            id: 1,
+            name: 'Service Offering',
+            description: ['A description'],
+        },
+        serviceAccessPoints: [],
+        physicalResources: [],
+        dataResources: [],
+    };
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
@@ -13,6 +32,7 @@ describe('ServiceCardComponent', () => {
 
         fixture = TestBed.createComponent(ServiceCardComponent);
         component = fixture.componentInstance;
+        component.service = mockService;
         fixture.detectChanges();
     });
 
